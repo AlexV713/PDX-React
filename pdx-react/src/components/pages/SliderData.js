2e9dc0e26fdcfca4a61672ec1fcbdf0a3ddbcb03
Alex Valerio <62916245+AlexV713@users.noreply.github.com>
2021-06-21 12:07:32 -0700

    Added Images to Slideshow
    I added all of the images to the slideshow and had to change the extension to jpg from jpeg. The slideshow was causing some sizing glitch, but I fixed it to keep it from resizing.

--- pdx-react/src/components/pages/SliderData.js
+++ pdx-react/src/components/pages/SliderData.js
@@ -1,22 +1,148 @@
 export const SliderData = [
   {
     image:
-      './images/kitchen.jpg'
+      './images/14.jpg'
   },
   {
     image:
-      './images/brick_patio.jpg'
+      './images/01.jpg'
   },
   {
     image:
-      './images/brick_patio2.jpg'
+      './images/02.jpg'
   },
   {
     image:
-      './images/floor_paint.jpg'
+      './images/03.jpg'
   },
   {
     image:
-      './images/new_floor.jpg'
+      './images/04.jpg'
+  },
+  {
+    image:
+      './images/05.jpg'
+  },
+  {
+    image:
+      './images/06.jpg'
+  },
+  {
+    image:
+      './images/07.jpg'
+  },
+  
+  {
+    image:
+      './images/08.jpg'
+  },
+  
+  {
+    image:
+      './images/09.jpg'
+  },
+  {
+    image:
+      './images/10.jpg'
+  },
+  
+  {
+    image:
+      './images/11.jpg'
+  },
+  
+  {
+    image:
+      './images/12.jpg'
+  },
+  
+  {
+    image:
+      './images/13.jpg'
+  },
+  
+  {
+    image:
+      './images/15.jpg'
+  },
+  
+  {
+    image:
+      './images/16.jpg'
+  },
+  
+  {
+    image:
+      './images/17.jpg'
+  },
+  {
+    image:
+      './images/18.jpg'
+  },
+  {
+    image:
+      './images/19.jpg'
+  },
+  {
+    image:
+      './images/20.jpg'
+  },
+  {
+    image:
+      './images/21.jpg'
+  },
+  {
+    image:
+      './images/22.jpg'
+  },
+  {
+    image:
+      './images/23.jpg'
+  },
+  {
+    image:
+      './images/24.jpg'
+  },
+  {
+    image:
+      './images/25.jpg'
+  },
+  {
+    image:
+      './images/26.jpg'
+  },
+  {
+    image:
+      './images/27.jpg'
+  },  {
+    image:
+      './images/28.jpg'
+  },  {
+    image:
+      './images/29.jpg'
+  },  {
+    image:
+      './images/30.jpg'
+  },  {
+    image:
+      './images/31.jpg'
+  },  {
+    image:
+      './images/32.jpg'
+  },  {
+    image:
+      './images/33.jpg'
+  },  {
+    image:
+      './images/34.jpg'
+  },  {
+    image:
+      './images/35.jpg'
+  },  {
+    image:
+      './images/36.jpg'
+  },  {
+    image:
+      './images/37.jpg'
   }
 ];
